ea973cff15c17b84e2b0dfed7eafe88b484f1a32
GerritRiesch94 <gerrit.riesch@live.de>
2023-01-03 13:03:12 +0100

    feat(angular-material): add simple toolbar with menu icon and app name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, MatIconModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })

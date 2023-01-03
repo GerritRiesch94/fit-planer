71ece92983573ff2a83e5449b4214e3c68c1349d
GerritRiesch94 <gerrit.riesch@live.de>
2023-01-03 16:14:22 +0100

    feat(navbar): add initial implementation of language switch

---
+++ src/app/navbar/language-switch/language-switch.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-language-switch',
+  templateUrl: './language-switch.component.html',
+  styleUrls: ['./language-switch.component.css'],
+})
+export class LanguageSwitchComponent {
+  public languages: { id: string; value: string }[] = [
+    { id: '0', value: 'en-US' },
+    { id: '1', value: 'de-DE' },
+  ];
+
+  constructor(private readonly translateService: TranslateService) {}
+
+  changeLanguage(selectedId: string) {
+    const matchingLanguage = this.languages.find((language) => language.id === selectedId);
+    if (matchingLanguage) {
+      this.translateService.use(matchingLanguage.value);
+    }
+  }
+}

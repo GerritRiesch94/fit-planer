c53556f43c9a66a555b5da139d099240692f91a7
GerritRiesch94 <gerrit.riesch@live.de>
2023-01-03 10:13:03 +0100

    ci(sonar-scan): remove test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
